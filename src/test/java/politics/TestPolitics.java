a460d824aa1c7ed44e8d359961052717d8c28c1d
theAmneziac <KidA424@gmail.com>
2017-08-26 16:32:35 -0700

    documenting integration test

--- src/test/java/politics/TestPolitics.java
+++ src/test/java/politics/TestPolitics.java
@@ -2,21 +2,30 @@ package politics;
 
 import junit.framework.TestCase;
 
+/**
+ * Integration test where elections are held and their outcomes verified.
+ */
 public class TestPolitics extends TestCase {
 
     public void setUp() {
+        // set seed for reproducible outcomes
         long seed = 123456789;
         Legislator.random.setSeed(seed);
         Citizen.random.setSeed(seed);
     }
 
+    /**
+     * Test that a legislative body can vote on a bill, and that the expected results are produced.
+     */
     public void testLegislativeBodyVote() {
 
+        // initialize LegislativeBody
         LegislativeBody legislativeBody = new LegislativeBody("Utopia Legislature");
         for (int j = 0; j < 10; j++) {
             legislativeBody.addLegislator(new Legislator("Some Important Person", Faction.RED));
         }
 
+        // initialize Bill
         Bill bill = new Bill(
                 "Economic Prosperity Act",
                 new String[]{
@@ -24,20 +33,32 @@ public class TestPolitics extends TestCase {
                 }
         );
 
+        // define expectations
         boolean[] expected = new boolean[] { false, false, false, true, true };
+
+        // run test 5 times
         for (int i = 0; i < 5; i++) {
+            // conduct election
             boolean result = legislativeBody.voteOnBill(bill);
+            // check result against expectation
             assertEquals(result, expected[i]);
         }
+
     }
 
-    public void testNoTieCandidateElection() {
+    /**
+     * Test that a NoTiesElection can be held where citizens vote on a list of candidates,
+     * and that the expected results are produced.
+     */
+    public void testNoTiesCandidateElection() {
 
+        // initialize citizens
         Citizen[] citizens = new Citizen[10];
         for (int j = 0; j < citizens.length; j++) {
             citizens[j] = new Citizen("Jane Doe");
         }
 
+        // initialize candidates
         final String office = "Some Important-Sounding Job Title";
         Candidate[] candidates = new Candidate[] {
                 new Candidate(String.format("Super Special Person 1"), office, Faction.RED),
@@ -45,13 +66,18 @@ public class TestPolitics extends TestCase {
                 new Candidate(String.format("Super Special Person 3"), office, Faction.GREEN)
         };
 
+        // define expectations
         int[] expected = new int[] { 1, 1, 2, 1, 2 };
 
+        // run test 5 times
         for (int i = 0; i < 5; i++) {
+            // construct NoTiesElection
             NoTiesElection<Candidate> election = new NoTiesElection<>(
                     new Election<>(candidates, citizens)
             );
+            // conduct election
             election.conductElection();
+            // check result against expectation
             assertEquals(election.getWinner(), candidates[expected[i]]);
         }
 
